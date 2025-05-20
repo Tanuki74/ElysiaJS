dc189749f9c012aae26da3637cf771d47d72cf8a
Tanuki74 <bannyjuanda@gmail.com>
2025-05-20 11:26:28 +0700

    validation type

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 
 class Note{
-  constructor(public data: string[] = ['Moonhalo']) {}
+  constructor(public data: string[] = ['Moonhalo', 'Bun', 'Elysia']) {}
 }
 
 const app = new Elysia()
       .use(swagger())
       .decorate('note', new Note())
       .get('/note', ({ note }) => note.data)
-      .get('/note/:index', ({ note, params: { index } }) => {
-        return note.data[index]
-      })
+      .get(
+            '/note/:index', 
+            ({ note, params: { index } }) => {
+              return note.data[index]
+            },
+            {
+              params: t.Object({
+                index: t.Number()
+              })
+            }
+          )
       .listen(3000);
 
 console.log(
