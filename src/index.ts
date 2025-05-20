302d69bc14d773b224ffb9bdd92190e47e7418d3
Tanuki74 <bannyjuanda@gmail.com>
2025-05-20 10:49:05 +0700

    adding swagger

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
 const app = new Elysia()
-      .get("/", () => "Hello Elysia")
-      .get("/hello", () => "Did you miss me?")
+      // .get("/", () => "Hello Elysia")
+      // .get("/hello", () => "Did you miss me?")
+      .use(swagger())
+      .get("/", ({ path }) => path)
+      .post("/hello", () => "Did you miss me?")
       .listen(3000);
 
 console.log(
